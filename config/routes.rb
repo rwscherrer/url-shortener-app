36316640b00eee5061cf36f943505bd89dcecab8
rwscherrer <rwscherrer@gmail.com>
2016-09-27 13:41:25 -0500

    added links route controller view and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
 
-    devise_scope :user do
-     authenticated :user do
-       root 'links#index', as: :authenticated_root
-     end
+  devise_scope :user do
+   authenticated :user do
+     root 'links#index', as: :authenticated_root
+   end
 
-     unauthenticated do
-       root 'devise/registrations#new', as: :unauthenticated_root
-     end
+   unauthenticated do
+     root 'devise/registrations#new', as: :unauthenticated_root
+   end
+  end
+
+  resources :links
+
+  resources :visits
+
+  get '/:slug' => 'visits#create'
+
+  get '/links' => 'links#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
